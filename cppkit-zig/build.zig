a4b1114af8c0c65e3361c1ae7b6d7d9e769de4f2
Dawid <dawidcxx@proton.com>
2025-11-09 16:08:33 +0100

    cppkit-zig: rough collection working

--- cppkit-zig/build.zig
+++ cppkit-zig/build.zig
@@ -55,7 +55,13 @@ fn makeCompileCommandsStep(step: *std.Build.Step, make_options: std.Build.Step.M
     }
 
     for (compile_command_entries.items) |entry| {
-        std.log.info("SUCCESS: Gathered cpp file {s}", .{entry.file});
+        const flags = try std.mem.join(alloc, ",", entry.flags);
+        defer alloc.free(flags);
+        const libs = try std.mem.join(alloc, ",", entry.linkObjects);
+        defer alloc.free(libs);
+        const headers = try std.mem.join(alloc, ",", entry.includes);
+        defer alloc.free(headers);
+        std.log.info("SUCCESS: Gathered cpp file '{s}' with flags '{s}' with headers '{s}' ", .{ entry.file, flags, headers });
     }
 }
 
@@ -66,26 +72,84 @@ fn gatherCompileCommandEntries(
 ) !void {
     for (step.dependencies.items) |dependency| {
         if (dependency.cast(std.Build.Step.Compile)) |compile_step| {
-            std.log.info("Dependency name={s}", .{dependency.name});
-            for (compile_step.root_module.link_objects.items) |link_object| {
-                switch (link_object) {
-                    .c_source_files => |csf| {
-                        for (csf.files) |f| {
-                            std.log.info(".c_source_files branch hit: {s}", .{f});
-                        }
-                    },
-                    .c_source_file => |csf| {
-                        std.log.info("Looking at {s}", .{csf.file.getDisplayName()});
-                        switch (csf) {
-                            else => {
-                                std.log.info("Looking at {s}", .{csf.file.getDisplayName()});
-                            },
-                        }
+            var headers: std.ArrayListUnmanaged([]const u8) = try .initCapacity(alloc, compile_step.root_module.include_dirs.items.len);
+            defer headers.deinit(alloc);
+            defer for (headers.items) |h| alloc.free(h);
+
+            for (compile_step.root_module.include_dirs.items) |d| {
+                switch (d.path) {
+                    .cwd_relative => |path| {
+                        headers.appendAssumeCapacity(try alloc.dupe(u8, path));
                     },
                     else => {},
+                }
+            }
+
+            var libraries: std.ArrayListUnmanaged([]const u8) = try .initCapacity(alloc, compile_step.root_module.link_objects.items.len);
+            defer libraries.deinit(alloc);
+            defer for (libraries.items) |l| alloc.free(l);
+
+            var source_files_and_flags: std.ArrayListUnmanaged([2][]const []const u8) = try .initCapacity(alloc, compile_step.root_module.link_objects.items.len * 2);
+            // defer source_files_and_flags.deinit(alloc);
+            // defer for (source_files_and_flags.items) |item| {
+            //     for (item[0]) |file| alloc.free(file);
+            //     for (item[1]) |flag| alloc.free(flag);
+            // };
+
+            for (compile_step.root_module.link_objects.items) |link_object| {
+                on_item: {
+                    switch (link_object) {
+                        .system_lib => |sl| {
+                            libraries.appendAssumeCapacity(sl.name);
+                        },
+                        .c_source_file => |cs| {
+                            const file = blk: {
+                                switch (cs.file) {
+                                    .cwd_relative => |path| {
+                                        break :blk try alloc.dupe(u8, path);
+                                    },
+                                    else => {
+                                        break :on_item;
+                                    },
+                                }
+                            };
+                            const file_container = try alloc.alloc([]const u8, 1);
+                            file_container[0] = file;
+                            const flags = try cloneStringSlice(alloc, cs.flags);
+                            try source_files_and_flags.append(alloc, .{ file_container, flags });
+                        },
+                        .c_source_files => |csf| {
+                            const flags = try cloneStringSlice(alloc, csf.flags);
+                            const files = try cloneStringSlice(alloc, csf.files);
+                            try source_files_and_flags.append(alloc, .{ files, flags });
+                        },
+                        else => {},
+                    }
+                }
+            }
+
+            for (source_files_and_flags.items) |source_file_and_flags| {
+                const files = source_file_and_flags[0];
+                const flags = source_file_and_flags[1];
+                for (files) |file| {
+                    const compile_commands_entry: CompileCommandsEntry = .{
+                        .file = file,
+                        .flags = flags,
+                        .includes = try headers.toOwnedSlice(alloc),
+                        .linkObjects = try libraries.toOwnedSlice(alloc),
+                    };
+                    try compile_command_entries.append(alloc, compile_commands_entry);
                 }
             }
         }
         try gatherCompileCommandEntries(alloc, dependency, compile_command_entries);
     }
+}
+
+fn cloneStringSlice(gpa: std.mem.Allocator, container: []const []const u8) error{OutOfMemory}![]const []const u8 {
+    const result = try gpa.alloc([]const u8, container.len);
+    for (container, 0..) |str, i| {
+        result[i] = try gpa.dupe(u8, str);
+    }
+    return result;
 }
